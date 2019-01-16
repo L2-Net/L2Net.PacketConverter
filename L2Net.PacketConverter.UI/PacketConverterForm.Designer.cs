e45338c7de335fb3230d32f09bf561d4b6acb69d
Alan Płócieniak <alan-null@outlook.com>
2019-01-16 21:56:45 +0100

    Added icon

--- L2Net.PacketConverter.UI/PacketConverterForm.Designer.cs
+++ L2Net.PacketConverter.UI/PacketConverterForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PacketConverterForm));
             this.rtbxHex = new System.Windows.Forms.RichTextBox();
             this.rtbxPlain = new System.Windows.Forms.RichTextBox();
             this.rbtnByte = new System.Windows.Forms.RadioButton();
@@ -156,6 +157,7 @@
             this.Controls.Add(this.rbtnByte);
             this.Controls.Add(this.rtbxPlain);
             this.Controls.Add(this.rtbxHex);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(568, 264);
             this.MinimumSize = new System.Drawing.Size(568, 264);
